20d12a86fb639717d47d170858916754bd3a0a8d
Marek Honzal <marek.honzal@gmail.com>
2021-11-02 18:38:33 +0100

    ticker, price, total stockholder equity, shares

--- roi_calculator/utils.py
+++ roi_calculator/utils.py
@@ -1,5 +1,25 @@
 import yfinance as yf
 
 
-def ticker_info(ticker):
-    return yf.Ticker(ticker).info
+def valuation_dictionary(ticker):
+    # Data from yf info method
+    basic_info = yf.Ticker(ticker).info
+    if basic_info['regularMarketPrice'] == None:
+        return None
+    symbol = basic_info['symbol']
+    price = basic_info['regularMarketPrice']
+    shares_outstanding = basic_info['sharesOutstanding']
+    # Data from yf balance_sheet method
+    total_stockholders_equity = total_stockholders_equity(ticker)
+    # Data from yf earnings method
+    # price_earnings_median =
+
+
+def total_stockholders_equity(ticker):
+    balance_sheet = yf.Ticker(ticker).balancesheet
+    return balance_sheet.loc['Total Stockholder Equity'][0]
+
+
+def price_earnings_median(ticker, shares_outstanding, price):
+    earnings = yf.Ticker(ticker).earnings
+    
--- roi_calculator/views.py
+++ roi_calculator/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import TickerForm
-from .utils import ticker_info
+from .utils import valuation_dictionary
+import yfinance as yf
 
 
 def ticker_form(request):
-
     if request.method == 'POST':
         form = TickerForm(request.POST)
         if form.is_valid():
             ticker = form.cleaned_data['ticker']
-        print(ticker)
-        print(ticker_info(ticker))
-
+            print(yf.Ticker(ticker).earnings)
+            print(yf.Ticker(ticker).financials)
+            print(yf.Ticker(ticker).quarterly_financials)
+            print(yf.Ticker(ticker).quarterly_balance_sheet)
     form = TickerForm()
     return render(request, 'search.html', {'form': form})
