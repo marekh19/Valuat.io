90e1de17105264aaa8fe827b3161eaff8d76c936
Marek Honzal <marek.honzal@gmail.com>
2021-11-30 17:10:37 +0100

    frontend updated slightly

--- roi_calculator/forms.py
+++ roi_calculator/forms.py
@@ -5,4 +5,4 @@ class TickerForm(forms.Form):
     ticker = forms.CharField(label="e.g. 'AMD' or 'AAPL'", max_length=6)
 
 class TickerFormSmall(forms.Form):
-    ticker = forms.CharField(max_length=6)
+    ticker = forms.CharField(max_length=6, widget=forms.TextInput(attrs={'placeholder': "e.g. 'AMD' or 'AAPL'"}))
